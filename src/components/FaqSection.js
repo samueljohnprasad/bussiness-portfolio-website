79edd07725e615a8823ad489dd9e5d94119edeb4
samueljohnprasad <samgolla.johnprasad777@gmail.com>
2021-05-14 13:48:44 +0530

    added FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function FaqSection() {
+    return (
+        <div className='faq'>
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className='question'>
+                <h4>How Do I Start?</h4>
+                <div className='answer'>
+                    <p>Lorem ipsum</p>
+                    <p>hwllo everyone</p>
+                </div>
+            </div>
+            <div className='question'>
+                <h4>Daily schedule</h4>
+                <div className='answer'>
+                    <p>Lorem ipsum</p>
+                    <p>hwllo everyone</p>
+                </div>
+            </div>
+            <div className='question'>
+                <h4>Different Payment Methods?</h4>
+                <div className='answer'>
+                    <p>Lorem ipsum</p>
+                    <p>hwllo everyone</p>
+                </div>
+            </div>
+            <div className='question'>
+                <h4>What product do you offer?</h4>
+                <div className='answer'>
+                    <p>Lorem ipsum</p>
+                    <p>hwllo everyone</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 //page components
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
 import ServicesSection from "../components/ServicesSection";
 export default function AboutUs() {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection/>
     </>
   );
 }
