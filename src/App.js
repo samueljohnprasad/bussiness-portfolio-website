99278ae85971887f537be729d3cc4368acb44fc9
samueljohnprasad <samgolla.johnprasad777@gmail.com>
2021-05-14 17:36:51 +0530

    added the Nav  bar and some stylings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
-import './App.css';
+
 import AboutUs from './pages/AboutUs'
 //global style
 import GlobalStyle from './components/GlobalStyle'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
      <AboutUs/>
     </div>
   );
--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { About } from "../pages/styles";
+import styled from "styled-components";
 
 export default function FaqSection() {
     return (
-        <div className='faq'>
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className='question'>
                 <h4>How Do I Start?</h4>
@@ -33,6 +35,34 @@ export default function FaqSection() {
                 </div>
             </div>
             
-        </div>
+        </Faq>
     )
 }
+
+const Faq=styled(About)`
+  display:block;
+  span{
+      display:block;
+  }
+  h2{
+      padding-botton:2rem;
+      font-weight:lighter;
+  }
+  .faq-line{
+      background:#cccccc;
+      height:0.2rem;
+      margin:2rem 0rem;
+      width:100%;
+
+  }
+  .question{
+      padding:3rem 0rem;
+      cursor:pointer;
+  }
+  .answer{
+      padding:2rem 0rem;
+      p{
+          padding:1rem 0rem;
+      }
+  }
+`;
